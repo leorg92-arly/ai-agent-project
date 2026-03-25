20b01b5bc61f823532ee6b7513665d9b1d2cb8bc
leorg92-arly <leo.rg92@icloud.com>
2026-03-25 10:29:34 -0600

    Verbose answer

--- main.py
+++ main.py
@@ -18,15 +18,21 @@ def main():
     # User text input
     parser = argparse.ArgumentParser(description="AI Code Agent")
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt` as the contents for Gemini call.
 
     # Message Content List
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    generate_content(client, messages)
+    messages = [types.Content(role="user", 
+                              parts=[types.Part(text=args.user_prompt)])
+                ]
+    
+    # Call Gemini
+    generate_content(client, messages, args.verbose)
 
-def generate_content(client, messages):
-    # Create a generative response
+# Create a generative response
+def generate_content(client, messages, is_verbose):
+    # Calling Gemini
     response = client.models.generate_content(
         model = 'gemini-2.5-flash', 
         contents = messages
@@ -37,12 +43,17 @@ def generate_content(client, messages):
         raise RuntimeError("Usage metadata is empty")
     
     # Print response and token usage metadata to terminal
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+    if is_verbose:
+        print(f"User prompt: {messages[0].parts[0].text}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+    else:
+        print(response.text)
 
 
 
 if __name__ == "__main__":
     main()
+
