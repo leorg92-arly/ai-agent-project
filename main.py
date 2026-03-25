c955c87cec6d8f8b51a1d773b4999edcc286aed8
leorg92-arly <leo.rg92@icloud.com>
2026-03-25 08:24:40 -0600

    User Response Parser

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
     load_dotenv()
@@ -13,10 +14,16 @@ def main():
     # Create a client for Google Gen AI
     client = genai.Client(api_key=api_key)
 
+    # User text input
+    parser = argparse.ArgumentParser(description="AI Coding Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt` as the contents for Gemini call.
+
     # Create a generative response
     response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model = 'gemini-2.5-flash', 
+        contents = args.user_prompt
     )
     
     #Token Usage Metadata
